85595ee2a8b12496680ff9ac3f7c8d338f492540
森田貴帆 <kiho.m@moritakihonoMacBook-Pro.local>
2017-08-20 15:17:20 +0900

    Initial Commit

---
+++ huriwake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  huriwake
+//
+//  Created by 森田貴帆 on 2017/08/20.
+//  Copyright © 2017年 森田貴帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
